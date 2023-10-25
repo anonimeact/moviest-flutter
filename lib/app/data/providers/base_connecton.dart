1988e0fe044414eb29e0a0fb95497ee710f98d31
anonimeact <anonimeact@gmail.com>
2023-10-26 00:34:41 +0700

    first init project

--- lib/app/data/providers/base_connecton.dart
+++ lib/app/data/providers/base_connecton.dart
@@ -1,17 +1,18 @@
 import 'package:get/get.dart';
 import 'package:get/get_connect/http/src/request/request.dart';
 
+import '../../../main.dart';
+
 class BaseService extends GetConnect {
   @override
-  void onInit() {
+  void onInit() async {
     httpClient.baseUrl = 'https://api.themoviedb.org/3/';
     httpClient.addRequestModifier((Request request) async {
       request.headers['accept'] = 'application/json';
-      request.headers['Authorization'] =
-          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiM2M1YTFjNWE3MzdkYTJlNWIwM2UzNmQ5ZTA0ZDc5ZSIsInN1YiI6IjY1MTNhYzAxMDQ5OWYyMDExYjA5NTIzNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iTfKIZPZ-XfGFo0c5dDCc7VO6gtIEWynyO36hgC8hTM';
+      request.headers['Authorization'] = 'Bearer $token';
       return request;
     });
-   
+
     httpClient.defaultContentType = "application/json";
     super.onInit();
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 import 'app/services/objectbox.dart';
+
 late ObjectBox objectbox;
+late String token;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   objectbox = await ObjectBox.create();
+  token = await rootBundle.loadString('assets/token.text');
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
